01299416097e4bcf047035b0d00d71ff75a0985d
Tommy Li <tommyli76278@gmail.com>
2022-12-04 18:47:47 -0800

    Completed Day 4 puzzle 2

---
+++ Nov 2022/Day 4/Puzzle 2/find_overlap_pairs_2.py
@@ -0,0 +1,22 @@
+file = open('..\\Puzzle 1\\input.txt')
+
+def find_overlap_pairs(handle):
+    total = 0
+
+    for line in handle:
+        (group_1, group_2) = line.rstrip().split(',')
+        (group_1_low, group_1_high) = group_1.split('-')
+        (group_2_low, group_2_high) = group_2.split('-')
+
+        if int(group_1_low) < int(group_2_low):
+            if int(group_1_high) >= int(group_2_low):
+                total += 1
+        elif int(group_1_low) > int(group_2_low):
+            if int(group_1_low) <= int(group_2_high):
+                total += 1
+        else:
+            total += 1
+
+    return total
+
+print(find_overlap_pairs(file))
